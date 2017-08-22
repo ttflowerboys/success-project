3f788d7cb026a6f534b2d9f98dfb0a1634cb33fe
threetown <threetowns@163.com>
2017-08-22 22:02:51 +0800

    [CONFIG] config databases information.

--- dbconfig.php
+++ dbconfig.php
@@ -1,17 +1,17 @@
 <?php
 
 $array1 = array(
-    # 数据库配置
-    'DB_TYPE'=>'mysql',
-	'DB_HOST'=>'127.0.0.1',
-	'DB_USER'=>'root',
-	'DB_PWD'=>'root',
-	'DB_NAME'=>'packet',
-	'DB_PORT'=>'',
-	'DB_PREFIX'=>'jack_',
+  # 数据库配置
+  'DB_TYPE'=>'mysql',
+  'DB_HOST'=>'127.0.0.1',
+  'DB_USER'=>'root',
+  'DB_PWD'=>'root',
+  'DB_NAME'=>'success',
+  'DB_PORT'=>'',
+  'DB_PREFIX'=>'jack_',
 
-	//'配置项'=>'配置值'
-	'DEFAULT_THEME' => 'default'
+  //'配置项'=>'配置值'
+  'DEFAULT_THEME' => 'default'
 
 );
 
