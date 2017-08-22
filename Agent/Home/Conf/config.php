7e91b4f29f008a631eeae88e4b1f84850bb0a2c5
threetown <threetowns@163.com>
2017-08-22 21:11:28 +0800

    Init: agent modules

---
+++ Agent/Home/Conf/config.php
@@ -0,0 +1,6 @@
+<?php
+
+require './dbconfig.php';
+return $array1;
+
+?>
---
+++ dbconfig.php
@@ -0,0 +1,18 @@
+<?php
+
+$array1 = array(
+    # 数据库配置
+    'DB_TYPE'=>'mysql',
+	'DB_HOST'=>'127.0.0.1',
+	'DB_USER'=>'root',
+	'DB_PWD'=>'root',
+	'DB_NAME'=>'packet',
+	'DB_PORT'=>'',
+	'DB_PREFIX'=>'jack_',
+
+	//'配置项'=>'配置值'
+	'DEFAULT_THEME' => 'default'
+
+);
+
+?>
