ff41e7ab24d601ec6221d69e1a126db6881263ed
threetown <threetowns@163.com>
2017-08-27 16:37:30 +0800

    [login] Add: javascript feature.

--- Agent/Home/Controller/IndexController.class.php
+++ Agent/Home/Controller/IndexController.class.php
@@ -3,8 +3,11 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends CommandController {
     public function index(){
+    	$rs = M('agent')->where(array('id'=>session('AgentId'),'phone'=>session('AgentPhone')))->find();
+    	$this->assign('rs',$rs);
         $this->display();
     }
+
 
     public function agent(){
         $this->display();
